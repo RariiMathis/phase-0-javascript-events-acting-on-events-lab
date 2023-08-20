9e8b198f0b06adb37bbf64eb836472fc40c94d4d
Rarii Mathis <juniormathis82@gmail.com>
2023-08-19 23:57:35 -0400

    listener game

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.background ="#000000";
+dodger.style.backgroundColor = "#ff69b4";
+dodger.style.bottom = "100px";
+dodger.style.bottom = "0px";
+dodger.style.left = "0px";
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+  });
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+    }
+  });
